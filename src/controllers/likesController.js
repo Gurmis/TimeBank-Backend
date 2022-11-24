981cce79ef58af6fbc949889171dea992da5eee1
Peter Girman <peter.girman@t-systems.com>
2022-11-24 10:47:30 +0100

    likes controller changed

--- src/controllers/likesController.js
+++ src/controllers/likesController.js
@@ -3,31 +3,43 @@ const helper = require("../helper");
 const config = require("../config/config");
 
 // GET BY ID
-async function getLikesByJobId(id) {
-  const rows = await db.query(
-    `SELECT COUNT(job_id) AS likes
-       FROM likes
-       WHERE job_id = ${id};`
-  );
-  const data = helper.emptyOrRows(rows);
-
-  return data;
+async function getLikesByJobId(req, res, next) {
+  try {
+    const id = req.params.id;
+    const rows = await db.query(
+      `SELECT COUNT(job_id) AS likes
+         FROM likes
+         WHERE job_id = ${id};`
+    );
+    const data = helper.emptyOrRows(rows);
+  
+    res.send(data);
+    
+  } catch (error) {
+    res.status(500).send({error});
+  }
 }
 
 // POST
-async function addLike(id) {
-  const newLike = await db.query(
-    `INSERT INTO likes (job_id, date_added)
-      VALUES
-      (${id}, (SELECT CURRENT_TIMESTAMP()));`
-  );
-
-  let message = "Error in adding like";
-  if (newLike.affectedRows) {
-    message = `Like for Job ID: ${id} successfully added`;
+async function addLike(req, res, next) {
+  try {
+    const id = req.params.id;
+    const newLike = await db.query(
+      `INSERT INTO likes (job_id, date_added)
+        VALUES
+        (${id}, (SELECT CURRENT_TIMESTAMP()));`
+    );
+  
+    let message = "Error in adding like";
+    if (newLike.affectedRows) {
+      message = `Like for Job ID: ${id} successfully added`;
+    }
+  
+    res.send({message});
+    
+  } catch (error) {
+    res.status(500).send({error})
   }
-
-  return { message };
 }
 
 module.exports = {
