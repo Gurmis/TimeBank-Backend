c161e6000dc9d273b157c5a0dee0c8ac5d94b48d
Peter Girman <peter.girman@t-systems.com>
2022-11-26 23:38:31 +0100

    forcessl

--- server.js
+++ server.js
@@ -42,6 +42,14 @@ app.use(cors({
   }
 }));
 
+var forceSsl = function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+app.use(forceSsl);
 
 app.use(cookieParser());
 app.use(express.json());
